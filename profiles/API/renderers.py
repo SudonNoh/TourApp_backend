014586b62131a5fd50cfaffde0bfd32d50a377e9
sudonnoh <sudonnoh@gmail.com>
2022-07-19 11:53:25 +0000

    update

--- profiles/API/renderers.py
+++
@@ -1,7 +0,0 @@
-from core.renderers import BaseJSONRenderer
-
-
-class ProfileJSONRenderer(BaseJSONRenderer):
-    object_label = 'profile'
-    pagination_object_label = 'profiles'
-    pagination_object_count = 'profilesCount'
--- profiles/API/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import ProfileRetrieveAPIView
-
-
-urlpatterns = [
-    path('<str:email>', ProfileRetrieveAPIView.as_view()),
-]
